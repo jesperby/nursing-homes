cc58aa41d557a9b51e957b0315ea19f13d0780fd
martent <marten@thavenius.se>
2013-12-13 14:54:24 +0100

    Styling of images

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -4,17 +4,17 @@ jQuery(document).ready(function($) {
   var $slideshow = $('#slideshow');
   if ($slideshow.length && $slideshow.find('.images > img').length > 1) {
 
-    var $count = $slideshow.find('.controls .count');
+    var $count = $slideshow.find('menu .count');
     var total = $slideshow.find('.images > img').length;
     var $slider = $slideshow.find('.images');
-    $slideshow.find('.controls').show();
+    $slideshow.find('menu').show();
 
     $slider.cycle({
       fx: 'fade',
       timeout: 0,
       speed: 200,
-      next: '#slideshow .controls .next, #slideshow img',
-      prev: '#slideshow .controls .prev',
+      next: '#slideshow menu .next, #slideshow img',
+      prev: '#slideshow menu .prev',
       swipe: true
     }).on('cycle-update-view', function(event, opts) {
       $count.text('Bild ' + (opts.currSlide + 1) + ' av ' + total);
