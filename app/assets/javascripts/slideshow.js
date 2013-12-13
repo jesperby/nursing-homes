a9a3fbdbaafee4efc46b8c3afd897aac9e0e0983
martent <marten@thavenius.se>
2013-12-13 14:20:21 +0100

    Upgrade to Cycle2 API

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -6,18 +6,26 @@ jQuery(document).ready(function($) {
 
     var $count = $slideshow.find('.controls .count');
     var total = $slideshow.find('.images > img').length;
+    var $slider = $slideshow.find('.images');
     $slideshow.find('.controls').show();
-    $count.text('Bild 1 av ' + total);
 
-    $slideshow.find('.images').cycle({
+    $slider.cycle({
       fx: 'fade',
       timeout: 0,
-      speed: 400,
+      speed: 200,
       next: '#slideshow .controls .next, #slideshow img',
       prev: '#slideshow .controls .prev',
-      onPrevNextEvent: function(isNext, index, slideElement) {
-        $count.text('Bild ' + (index + 1) + ' av ' + total);
-        $slideshow.css('z-index', '0');
+      swipe: true
+    }).on('cycle-update-view', function(event, opts) {
+      $count.text('Bild ' + (opts.currSlide + 1) + ' av ' + total);
+      $slideshow.css('z-index', '0');
+    });
+
+    $(document).keyup(function (event) {
+      if (event.keyCode == 37) {
+        $slider.cycle('prev');
+      } else if (event.keyCode == 39) {
+        $slider.cycle('next')
       }
     });
   }
