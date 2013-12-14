51080e9b927acc980f1593cf99ad083e15d85243
martent <marten@thavenius.se>
2013-12-14 12:59:11 +0100

    Flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,18 +15,18 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         redirect_to root_path, :notice => "Nu är du inloggad"
       else
-        flash[:alert] = "Du har inte behörighet till systemet"
+        flash[:warning] = "Du har inte behörighet till systemet"
         redirect_to apply_user_path
       end
     else
-      flash[:alert] = "Fel användarnamn eller lösenord"
+      flash[:warning] = "Fel användarnamn eller lösenord"
       render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Nu är du utloggad"
+    flash[:success] = "Nu är du utloggad"
     redirect_to :controller => "nursing_homes" , :action => "index", :protocol => "http"
   end
 end
